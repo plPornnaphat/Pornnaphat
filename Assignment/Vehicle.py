6f2aa524d6b495b9db7e8c8e65d129ff2bb3dd9c
pornnaphat <pornnaphat.h@rmutsvmail.com>
2022-06-26 22:04:08 +0700

    Test upload to githud

---
+++ Assignment/Vehicle.py
@@ -0,0 +1,20 @@
+"""
+student Name:pornnaphat homket
+ID :044
+Group :mit212
+"""
+
+
+
+class Vehicle:
+    def __init__(self,brandname,model,color,max_speed,price):
+        # object attributes
+        self.brandname = brandname
+        self.model = model
+        self.color = color
+        self.max_speed = max_speed
+        self.price = price
+
+    def Vehicle_detail(self):
+        print(f'brandname:{self.brandname} model: {self.model} '
+              f'color:{self.color} max_speed:{self.max_speed} price: {self.price}  THB')
